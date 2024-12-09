ad391712a9a2d4fde3f8eabc5b7fc2d6fa8d7315
tritrans <tritranminh484@gmail.com>
2024-12-09 08:17:13 -0800

    finish

---
+++ nhom10_CosmeticsShop/CosmeticsShop/CosmeticsShop/CosmeticsShop/Controllers/CartController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CosmeticsShop.Models;
+using CosmeticsShop.DBContext;
+
+namespace CosmeticsShop.Controllers
+{
+    
+
+    public class CartController : Controller
+    {
+        public CartController()
+        {
+            // Pass data to layout
+            CategoryDB categoryDB = new CategoryDB();
+            this.ViewData["categories"] = categoryDB.GetCategories();
+        }
+        // GET: Cart
+        public ActionResult ShowCart()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SelectItem(CartItemSelected data)
+        {
+            TempData["ItemSelected"] = data.ItemSeleted;
+            return Json(new { redirectToUrl = Url.Action("ShowCheckout", "Checkout") });
+        }
+
+    }
+}
---
+++ nhom10_CosmeticsShop/CosmeticsShop/CosmeticsShop/CosmeticsShop/ViewModel/RegisterVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using CosmeticsShop.Models;
+
+namespace CosmeticsShop.ViewModel
+{
+    public class RegisterVM : User
+    {
+    }
+}
